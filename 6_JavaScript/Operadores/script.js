37fd8711927280570bb059ce1bd026af28050900
Lucas Vecchi Barcelos <lucasvecchibarcelos@gmail.com>
2024-07-03 15:11:29 -0300

    js operadores

---
+++ 6_JavaScript/Operadores/script.js
@@ -0,0 +1,15 @@
+// Operadores
+// Number
+console.log(typeof 2);
+console.log(typeof 5.14);
+console.log(typeof  -127);
+// Operador Aritméticos
+console.log(5+5);
+console.log(5+(2*5));
+console.log(25/5);
+// Special numbers
+console.log(typeof Infinity);
+console.log(typeof -Infinity);
+console.log(12 * "lvb");  //resultado NAN
+
+// Strings
