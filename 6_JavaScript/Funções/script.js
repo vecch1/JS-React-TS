6b406161f449fb8767354626d7d28d5e6672788a
Lucas Vecchi Barcelos <lucasvecchibarcelos@gmail.com>
2024-07-16 18:24:10 -0300

    arrow function/argumento default

--- 6_JavaScript/Funções/script.js
+++ 6_JavaScript/Funções/script.js
@@ -63,5 +63,75 @@ function escopoAninhado(){
     }
     console.log(m)
 }
-
+console.log(m);
 escopoAninhado(); 
+
+// arrow function  /  forma de criar funtions, usada em frameworks
+const testArrow = () => {
+    console.log("Esta é uma arrow function")
+};
+
+testArrow();
+
+const parOuimpar = (n) => {
+    if(n % 2 === 0){
+        console.log("par");
+        return;
+    }
+
+    console.log("impar");
+};
+
+parOuimpar(5);
+parOuimpar(10);
+
+// Arrow function + resumida
+// com {} e return
+const raizQuadrada = (x) =>{
+    return x * x
+}
+console.log(raizQuadrada(4));
+
+// sem {} e return
+const raizQuadrada2 = (x) => x * x
+console.log(raizQuadrada2(5));
+console.log(raizQuadrada2(12));
+
+//  paramentro opcional
+const multiplication = function (m, n){
+    if (n === undefined){
+        return m * 2;
+    }else {
+       return  m * n;
+    }
+}
+console.log(multiplication(5));
+console.log(multiplication(2, 4));
+
+const greeting = (name) => {
+    if(!name){
+        console.log("ola")
+        return
+    }
+        console.log(`ola ${name}`)
+}
+
+greeting ();
+greeting("Lucas");
+
+// Argumento default  / tem valor prévio
+
+const customGreeting = (name, greet = "ola") => {
+    return `${greet}, ${name} `
+}
+console.log(customGreeting("lucas"));
+console.log(customGreeting("lucas", "bom dia"));
+
+
+const repeatText = (text, repeat = 2) => {
+    for(let i =  0; i < repeat; i++){
+        console.log(text)
+    }
+};
+repeatText("testando");
+repeatText("agora repete 5 vezes", 5);
