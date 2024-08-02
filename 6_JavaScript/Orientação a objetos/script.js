44c5f56a896f1d46c6c46610da2096a7db19f872
Lucas Vecchi Barcelos <lucasvecchibarcelos@gmail.com>
2024-08-02 12:33:54 -0300

    classes prototype

--- 6_JavaScript/Orientação a objetos/script.js
+++ 6_JavaScript/Orientação a objetos/script.js
@@ -45,3 +45,75 @@ console.log(Object.getPrototypeOf(myObject));
 
 const mySecondObject = Object.create(myObject)
 console.log(mySecondObject);
+
+// classes / prototypes sao originados de uma classe / js ja possui classes mas podemos criar as nossas
+const cachorro = {
+    raça: null,
+    patas: 4
+}
+const pastorAlemao = Object.create(cachorro)
+pastorAlemao.raça = "Pastor Alemão"
+console.log(pastorAlemao)
+
+console.log(pastorAlemao.patas)
+
+const bulldog = Object.create(cachorro)
+bulldog.raça = "Bulddog"
+console.log(bulldog)
+
+// Funçoes como classe 
+function criarCachorro (nome,raça){
+    const cachorro = {}
+
+    cachorro.nome = nome
+    cachorro.raça= raça
+
+    return cachorro
+}
+const bob = criarCachorro("Bob", "Vira-lata");
+console.log(bob)
+console.log(Object.getPrototypeOf(bob))
+
+// funçoees como construtoras / semelhante ao anterior com palavra "new" para instaciar ovos obj
+function Cachorro(nome,raça){
+    this.nome = nome
+    this.raça = raça
+}
+const husky = new Cachorro("Ozzy", "Husky")
+console.log(husky)
+
+// metodos de funçoes construtoras
+Cachorro.prototype.uivar = function() {
+    console.log("Auuu")
+}
+
+husky.uivar();
+
+// classes em js / nas versoes atuais do js abandonamos functions e utilizamos classes
+class CachorroClasse {
+    constructor(nome,raça){
+        this.nome = nome
+        this.raça = raça
+    }
+}
+const slinky = new CachorroClasse ("Slinky", "Labrador");
+console.log(slinky)
+
+// mais sobre classes 
+class Caminhão {
+    constructor (eixos, cor){
+        this.eixos = eixos
+        this.cor = cor
+    }
+
+    descreverCaminhão(){
+        console.log(`este caminhao tem ${this.eixos} eixos e é da cor ${this.cor}`)
+    }
+}
+const scania = new Caminhão (6, "vermelha")
+console.log(scania)
+
+scania.descreverCaminhão();
+
+const c2 = new Caminhão(4, "preta")
+console.log(c2)
