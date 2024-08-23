6f5b0d0f71d0bcd677ddfb52417b3cb5019cb073
Lucas Vecchi Barcelos <lucasvecchibarcelos@gmail.com>
2024-08-23 09:43:12 -0300

    appendchild

--- 6_JavaScript/JavaScript e navegador/script.js
+++ 6_JavaScript/JavaScript e navegador/script.js
@@ -31,3 +31,26 @@ console.log(productstQuery)
 
 const mainContainer = document.querySelector("#main-container")
 console.log(mainContainer)
+
+// insert before / cria um elemento antes de outro elemento
+const p = document.createElement("p")
+
+const header = title.parentElement;
+
+header.insertBefore(p, title)
+
+
+// appendchild / add elementro dentro de outro
+const navLinks = document.querySelector("nav ul")
+
+const li = document.createElement("li")
+
+navLinks.appendChild(li)
+
+// replace child / trocar elemento
+const h2 = document.createElement("h2")
+
+h2.textContent = "meu novo titulo"
+
+header.replaceChild(h2, title)
+
