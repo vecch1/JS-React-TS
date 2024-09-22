4e48948cfe628ca3d8ba50bcb483afeb361fe586
Lucas Vecchi Barcelos <lucasvecchibarcelos@gmail.com>
2024-09-22 15:30:08 -0300

    inserindo dados

--- 8_Axios/script.js
+++ 8_Axios/script.js
@@ -1,2 +1,41 @@
 // Axios
 console.log(axios);
+
+// primeiro request
+const getData = async() => {
+    try {
+        
+        const response = await axios.get("https://jsonplaceholder.typicode.com/users");
+        
+        console.log(response)  
+
+        return response.data;
+    } catch (error) {
+        console.log(error)
+    }
+}
+getData()
+
+// imprimindo dados na tela
+const container = document.querySelector("#user-container");
+const printData = async() => {
+    const data = await getData();
+
+    console.log(data);
+
+    data.forEach((user) => {
+        const div = document.createElement("div");
+        const nameElement = document.createElement("h2");
+        nameElement.textContent = user.name;
+        div.appendChild(nameElement);
+
+        const emailELement = document.createElement("p")
+        emailELement.textContent = user.email
+        div.appendChild(emailELement);
+
+        container.appendChild(div);
+    });
+
+};
+
+printData();
