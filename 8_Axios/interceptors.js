99711bb143fc1eebbc3ab04aecb0af6f5994d221
Lucas Vecchi Barcelos <lucasvecchibarcelos@gmail.com>
2024-09-30 13:10:53 -0300

    finalizando axios

---
+++ 8_Axios/interceptors.js
@@ -0,0 +1,21 @@
+// requisiçao
+postsFetch.interceptors.request.use(
+    function(config){
+        console.log("Antes da requisição..")
+        return config;
+    },
+    function(error){
+        return Promise.reject(error)
+    }
+)
+
+// resposta
+postsFetch.interceptors.response.use(
+    function(response){
+        console.log("Antes da resposta..")
+        return response;
+    },
+    function(error){
+        return Promise.reject(error)
+    }
+)
