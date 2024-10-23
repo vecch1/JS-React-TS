1650de1daa579be2c25b959477ec079e5b68c86a
Lucas Vecchi Barcelos <lucasvecchibarcelos@gmail.com>
2024-10-23 16:53:06 -0300

    incio js

--- Projeto Password Generate/script.js
+++ Projeto Password Generate/script.js
@@ -1,3 +1,12 @@
-const Name = document.querySelector("#nome");
-const Email = document.querySelector("#email");
-const Password = document.querySelector("#senha");
+const Name = document.getElementById("#nome");
+const Email = document.getElementById("#email");
+const Password = document.getElementById("#senha");
+const ConfirmaSenha = document.getElementById("#confirmaSenha")
+const Cadastrar_BTN = document.getElementById("#submit-btn")
+const GeneratePassword = document.getElementById("#genarate-password")
+
+
+if (Password.value !== ConfirmaSenha.value){
+    console.log("A  senha e a confirmação de senha não são iguais");
+alert("A senha e a confirmação de senha não são iguais");
+}
