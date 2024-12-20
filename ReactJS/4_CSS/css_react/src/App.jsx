82e4a0e8df80f086df399b2a7e3251001fe69538
Lucas Vecchi Barcelos <lucasvecchibarcelos@gmail.com>
2024-12-20 13:34:28 -0300

    css react

---
+++ ReactJS/4_CSS/css_react/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+      <div className="app">
+       {/*1 -  CSS Global */}
+      <h1>CSS no React</h1>
+      </div>
+     
+
+  )
+}
+
+export default App
