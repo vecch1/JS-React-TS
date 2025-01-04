1b2d86920aaef5b1212acc1f3f82eb06f13308b8
Lucas Vecchi Barcelos <lucasvecchibarcelos@gmail.com>
2025-01-04 19:28:18 -0300

    form

---
+++ ReactJS/5_Form/form/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import MyForm from './components/MyForm'
+
+
+function App() {
+  return (
+
+      <div className='App'>
+        <h1>Form em React</h1>
+        <MyForm />
+      </div>
+    
+  )
+}
+
+export default App
---
+++ ReactJS/5_Form/form/src/components/MyForm.jsx
@@ -0,0 +1,18 @@
+import "./MyForm.css";
+
+ const MyForm = () => {
+  return (
+    <div>
+    {/* criaçao de form */}
+    <form>
+        <div>
+            <label htmlFor="name">Nome:</label>
+            <input type="text" name="name"  placeholder="Digite seu nome" />
+        </div>
+        <input type="submit" value="Enviar" />
+    </form>
+</div>
+  )
+}
+
+export default MyForm;
