cff526a1125b9ed465d7a314e3574983e8fc2349
Lucas Vecchi Barcelos <lucasvecchibarcelos@gmail.com>
2025-01-06 15:02:13 -0300

    onchange

--- ReactJS/5_Form/form/src/components/MyForm.jsx
+++ ReactJS/5_Form/form/src/components/MyForm.jsx
@@ -1,14 +1,30 @@
 import "./MyForm.css";
 
+import { useState } from "react";
+
  const MyForm = () => {
+// genrenciamento de dados
+const [name, setName] = useState();
+const [email, setEmail] = useState();
+
+const handleName = (e) => {
+  setName(e.target.value);
+};
+console.log(name)
+
   return (
     <div>
     {/* criaçao de form */}
     <form>
         <div>
             <label htmlFor="name">Nome:</label>
-            <input type="text" name="name"  placeholder="Digite seu nome" />
+            <input type="text" name="name"  placeholder="Digite seu nome" onChange={handleName} />
         </div>
+        {/* label envolvendo input */}
+            <label>
+              <span> E-mail</span>
+              <input type="text"  name="email" placeholder="Digite seu e-mail" onChange={(e) => setEmail(e.target.value) } />
+            </label>
         <input type="submit" value="Enviar" />
     </form>
 </div>
