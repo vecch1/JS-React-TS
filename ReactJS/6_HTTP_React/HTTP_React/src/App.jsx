1a0c055c5fd90d85a9e8c377737e1eabb9d07a55
Lucas Vecchi Barcelos <lucasvecchibarcelos@gmail.com>
2025-01-25 23:59:48 -0300

    envio de dados

--- ReactJS/6_HTTP_React/HTTP_React/src/App.jsx
+++ ReactJS/6_HTTP_React/HTTP_React/src/App.jsx
@@ -18,7 +18,33 @@ function App() {
       }
       getData();
 
-    }, [] )
+    }, [] );
+
+    // 2 - envio de dados
+    const [name, setName] = useState("")
+    const [price, setPrice] = useState("")
+
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+
+      const product = {
+        name,
+        price
+      }
+      const res = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+      },
+      body: JSON.stringify(product),
+        }) ;
+
+      // 3 - carregamento de dados 
+      const addedProduct = await res.json()
+
+      setProducts((prevProducts) => [...prevProducts, addedProduct])
+
+    };
 
   return (
   
@@ -30,6 +56,20 @@ function App() {
             <li key={product.id}>{product.name} - R$ {product.price}</li>
           })}
         </ul>
+        {/* envio de dados */}
+        <div className="add-product">
+          <form onSubmit={handleSubmit} >
+          <label>
+            <span>Nome</span>
+            <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+          </label>
+          <label>
+            <span>Preço</span>
+            <input type="text" value={price} onChange={(e) => setPrice(e.target.value)} />
+          </label>
+          <input type="submit"  value="Enviar" />
+          </form>
+        </div>
       </div>
     
   )
